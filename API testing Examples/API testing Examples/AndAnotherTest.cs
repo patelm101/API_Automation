bfe1a403ec492d08a17b952df9ee0c020ff926be
patelm101 <39886398+patelm101@users.noreply.github.com>
2024-02-03 11:04:38 -0500

    Feb324 (#1)

---
+++ API testing Examples/API testing Examples/AndAnotherTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_testing_Examples
+{
+    class AndAnotherTest
+    {
+        [TestFixture]
+        public class NUnitDemo
+        {
+            [Test]
+            public void TestMethodSum()
+            {
+
+            }
+        }
+
+    }
+}
